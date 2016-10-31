407c4093856e0ae3faf743a9334109d73b046fd2
Rejia <darejian@gmail.com>
2016-10-31 21:28:51 +0200

    completed-set-ships-manually
    Function set_ships_manually complete (no checks for input data).

--- main_sea_battle.py
+++ main_sea_battle.py
@@ -6,6 +6,8 @@ boardPlayer2 = []
 
 empty_cell = 0 #value which marks cell as empty
 board_size = 10 #size of the board (size x size)
+deckShips = [4,3,2,1] #decks which ships can consist of
+numberOfShipsPerDeck = [1,2,3,4] # number of ships allowed for appropriate deck (matches with deck array)
 
 #FUNCTIONS
 
@@ -28,54 +30,28 @@ def clear_board(board):
             board[row][column] = 0
 
 #set ships manually
-def set_ships_manually(board):
+def set_ships_manually(board,deck,numberOfShips):
     print("Place a ship")
-    deck = [4,3,2,1]
     for j in range(len(deck)):
-        TopRow = int(input("Top row: "))
-        LeftCol = int(input("Left column: "))
-        sOrientation = raw_input("Orientation: h - horizontal, v - vertical: ")
-        if sOrientation == 'h':
-            for i in range(deck[j]): 
-                board[TopRow-1][LeftCol+i-1] = 1
-        else:
-            for i in range(deck[j]):
-                board[TopRow+i-1][LeftCol-1] = 1
-        for row in board:
-            print(row)
+        for n in range(numberOfShips[j]):
+            TopRow = int(input("Top row: "))
+            LeftCol = int(input("Left column: "))
+            sOrientation = raw_input("Orientation: h - horizontal, v - vertical: ")
+            if sOrientation == 'h':
+                for i in range(deck[j]): 
+                    board[TopRow-1][LeftCol+i-1] = 1
+            else:
+                for i in range(deck[j]):
+                    board[TopRow+i-1][LeftCol-1] = 1
+            print_board(board)
     
 
 #set ships automatically
+#shoot
 
 print("This is your board. Empty for now:")
 
 create_board(boardPlayer1) 
 print_board(boardPlayer1)
-set_ships_manually(boardPlayer1)
-
-
-
-#print("Now place your ships. Format: x y d")
-
-#PLACE 4x SHIP
-
-
-
-
-
-print_board(boardPlayer1)
+set_ships_manually(boardPlayer1,deckShips,numberOfShipsPerDeck)
 clear_board(boardPlayer1)
-
-
-'''
-print("Place 4x ship")
-print("Place 3x ship")
-print("Place another 3x ship")
-print("Place 2x ship")
-print("Place another 2x ship")
-print("Place another 2x ship")
-print("Place 1x ship")
-print("Place another 1x ship")
-print("Place another 1x ship")
-print("Place another 1x ship")
-'''
