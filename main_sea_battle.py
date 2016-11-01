3b91f9b2275a08c29ea3bbd429b88459d2250e1b
Darejian <darejian@gmail.com>
2016-11-01 02:30:51 +0200

    check ship decks vertically
    If enemy ship is hit, check vertically to define if the ship is killed or not.

--- main_sea_battle.py
+++ main_sea_battle.py
@@ -1,13 +1,26 @@
 print "Welcome to Sea Battle Game"
 
 #MAIN VARIABLES
-boardPlayer1 = []
+boardPlayer1 = [[],[]]
+#temporary initiation for debug purposes
+boardPlayer1 = [[1,0,0,0,0,0,0,0,0,0],
+                [1,0,0,0,0,0,0,0,0,0],
+                [1,0,0,0,0,0,0,0,0,0],
+                [1,0,0,1,0,0,0,0,0,0],
+                [0,0,0,1,0,0,0,0,0,0],
+                [0,0,0,1,0,0,0,0,0,0],
+                [0,0,0,1,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0]]
+
 boardPlayer2 = []
 
 empty_cell = 0 #value which marks cell as empty
 board_size = 10 #size of the board (size x size)
 deckShips = [4,3,2,1] #decks which ships can consist of
 numberOfShipsPerDeck = [1,2,3,4] # number of ships allowed for appropriate deck (matches with deck array)
+willShoot = "y" #does player want to shoot (y -yes, otherwise no)
 
 #FUNCTIONS
 
@@ -18,10 +31,29 @@ def create_board(board):
         for column in range(board_size): 
             board[row].append(empty_cell) 
 
+#initiate board (for debug purposes) - DOES NOT WORK, REASON NOT FOUND YET
+def initiate_board(board):
+    board = [[1,0,0,0,0,0,0,0,0,0],
+             [1,0,0,0,0,0,0,0,0,0],
+             [1,0,0,0,0,0,0,0,0,0],
+             [1,0,0,1,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0],
+             [0,0,0,1,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0]]
+
+    
 #print board
 def print_board(board):
+    x = ["",0,1,2,3,4,5,6,7,8,9,10]
+    print(x)
+    i=0
     for row in board:
-        print(row)
+        print( i, row)
+        i+=1
+        
 
 #clear board from ships
 def clear_board(board):
@@ -48,10 +80,73 @@ def set_ships_manually(board,deck,numberOfShips):
 
 #set ships automatically
 #shoot
+def shoot(board):
+    hitFlag = 0 #flag marking if the player missed (0) or hit (1) enemy's ship
+    Ship = []
+    iDeck = 0
+    aliveDecks = 0
+    row = 0
+    col = 0
+    
+    print("Shoot!")
+    targetRow = int(input("Target row: "))
+    targetCol = int(input("Target column: ")) #if the cell is empty
+    if board[targetRow][targetCol] == 0: 
+        board[targetRow][targetCol] = "."
+        print("Missed")
+    else:
+        #BUG: case when cell == x is not processed
+        #case when cell == 1:
+        if board[targetRow][targetCol] == 1:   
+            board[targetRow][targetCol] = "x"
+            #if the cel is not empty
+            #build vertical array of potential ship decks
+            i = 0
+            for row in range(targetRow-3,targetRow+4): # from -(MaxDeckSize-1) rows till +(MaxDeckSize-1) rows. BUG: MaxDeckSize not parametrized. 
+                print(targetRow,row, targetCol)
+                if board[row][targetCol] == 1 or board[row][targetCol] == "x":
+                    Ship.append([]) #add array cell to store a pair of coordinates
+                    Ship[i].append(row)
+                    Ship[i].append(targetCol)
+                    print(row,targetCol,Ship)
+                    i+=1
+            #check all items in the vertical array
+            iDeck = len(Ship)
+            #BUG: we don't check if the decks are sequential
+            if iDeck > 1: #if number of _sequential_ decks vertically > 1, then the ship is oriented vetically
+                i = 0
+                for i in range(iDeck):
+                    if board[Ship[i][0]][Ship[i][1]] == 1:
+                        aliveDecks+=1
+            if aliveDecks > 0:
+                print("Hit! Shoot again!")
+            else:
+                print("KILLED! Shoot again!")
+            
+            
+            #find sub-array of decks
+            #if sub-array of decks length is ==1, then
+                #build horisontal array of potential ship decks
+                    #if horisonal sub-array length ==1, then KILLED (only target cell )
+                    #else
+            #else (if vertical sub-array of decks length >1)
 
+         
+#BODY
+'''
 print("This is your board. Empty for now:")
-
-create_board(boardPlayer1) 
+create_board(boardPlayer1)
 print_board(boardPlayer1)
-set_ships_manually(boardPlayer1,deckShips,numberOfShipsPerDeck)
+'''
+print("Board initiated:")
+#initiate_board(boardPlayer1)
+print_board(boardPlayer1)
+#set_ships_manually(boardPlayer1,deckShips,numberOfShipsPerDeck)
+
+#shoot cycle
+while willShoot == "y":
+    shoot(boardPlayer1)
+    print_board(boardPlayer1)
+    willShoot = raw_input("Shoot again? y/n: ")
+    
 clear_board(boardPlayer1)
